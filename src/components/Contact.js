42d2a523f6f6c7ae4c49f21f9bba09958e8f15f8
Catalina Cueto (Cat) <catacueto@gmail.com>
2022-08-26 11:14:36 -0400

    adding helpers for email validation

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,36 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function Contact() {
+function ContactForm() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleInputChange = (event) => {
+    const { target } = event;
+    const inputType = target.name;
+    const inputValue = target.value;
+
+    if (inputType === "name") {
+      setName(inputValue);
+    } else if (inputType === "email") {
+      setEmail(inputValue);
+    } else {
+      setMessage(inputValue);
+    }
+  };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+
+    if (!name || !email || !message) {
+      setErrorMessage("Please fill in the missing field");
+      return;
+    } else {
+      setErrorMessage(
+        `Thank you, ${name}! I'll get back to you as soon as I can 🌟 `
+      );
+    }
+
+    setName("");
+    setEmail("");
+    setMessage("");
+  };
+
   return (
     <section className="contact-section" id="contact">
       <div className="container">
         <h2 className="section-title contact-title">contact</h2>
         {/* <div className="line-under-contact"></div> */}
         <div className="subhead-contact">Have questions or want to collab?</div>
-        <form
-          className="contact-form"
-          action="https://formsubmit.co/3d283826683bb386dfa1cf7ad3fb03b4"
-          method="POST"
-        >
+        <form className="contact-form">
           <input
             className="inputs"
+            value={name}
+            name="name"
             placeholder="Name"
             type="text"
-            name="name"
+            onChange={handleInputChange}
             required
           ></input>
           <input
             className="inputs"
+            value={email}
+            name="email"
             placeholder="Email"
             type="email"
-            name="email"
+            onChange={handleInputChange}
             required
           ></input>
           <textarea
             className="inputs message"
+            value={message}
+            name="message"
             placeholder="Enter Your Message"
             type="text"
-            name="message"
+            onChange={handleInputChange}
             required
           ></textarea>
           <input type="hidden" name="_captcha" value="false"></input>
@@ -39,9 +77,18 @@ export default function Contact() {
             type="submit"
             id="submitBtn"
             value="SUBMIT"
+            onClick={handleFormSubmit}
           ></input>
         </form>
+        <br></br>
+        {errorMessage && (
+          <div>
+            <p className="done-text">{errorMessage}</p>
+          </div>
+        )}
       </div>
     </section>
   );
 }
+
+export default ContactForm;
